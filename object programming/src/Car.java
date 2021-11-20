f897aeba58540651a5225b8284af7ecaacf0dc60
Krzysiek <kkk96@vp.pl>
2021-11-20 11:46:11 +0100

    New commit

---
+++ object programming/src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    public void startEngine(){
+        System.out.println("engine is on");
+    }
+    public void stopEngine(){
+        System.out.println("engine is off");
+    }
+}
--- object programming/src/Main.java
+++ object programming/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args){
 
+        Car audi = new Car();
+        audi.startEngine();
+        audi.stopEngine();
     }
 
 }
